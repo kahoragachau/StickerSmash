4c36f4541e201ae879f6ba5854f560c761664f4d
kahoragachau <joelkahoragachau6@gmail.com>
2024-03-08 10:07:27 +0300

    feat: Add expos Image Launcher dependency

--- App.js
+++ App.js
@@ -3,19 +3,39 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    });
+
+    if(!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any Image');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View styles={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer 
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
-      {/* <Text style={styles.textStyles}>Open up App.js to start working on your app!</Text> */}
 
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
